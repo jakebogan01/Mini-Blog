1ab9f2f76400f354c4992ccf4cf5a615eb136a66
Jake Bogan <jakebogan01@gmail.com>
2022-10-02 05:16:37 -0400

    adds delete functionality and 404 page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,19 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
     const {data:blog, isPending, error} = useFetch(' http://localhost:8000/blogs/' + id);
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: "DELETE"
+        })
+            .then(() => {
+                navigate('/');
+            })
+    }
 
     return (
         <div>
@@ -14,6 +24,7 @@ const BlogDetails = () => {
                     <h2 className="text-xl text-[#f1356d] mb-2.5">{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div className="my-5">{blog.body}</div>
+                    <button className="bg-[#f1356d] text-white border-0 p-2 rounded-lg cursor-pointer" onClick={handleClick} type="button">Delete</button>
                 </article>
             )}
         </div>
