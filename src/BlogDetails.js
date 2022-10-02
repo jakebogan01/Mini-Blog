b1feabbf4e2ac57624714c401e1440a087d526b2
Jake Bogan <jakebogan01@gmail.com>
2022-10-02 03:13:19 -0400

    adds blog details page and template

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,21 @@
 import {useParams} from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
+    const {data:blog, isPending, error} = useFetch(' http://localhost:8000/blogs/' + id);
 
     return (
         <div>
-            <h2>Blog Details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2 className="text-xl text-[#f1356d] mb-2.5">{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div className="my-5">{blog.body}</div>
+                </article>
+            )}
         </div>
     );
 }
